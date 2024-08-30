1151bf0dcb45c119c6a84ea26e66799ad62b5add
Smodolin <simon.dumfarth@gmail.com>
2024-08-30 11:57:26 +0200

    add word counter

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+def count_words(text):
+    words = text.split()
+    return len(words)
+
 def main():
 
     path_to_file = "books/frankenstein.txt"
@@ -6,6 +10,8 @@ def main():
         file_contents = f.read()
 
     print(file_contents)
+    word_count = count_words(file_contents)
+    print(f"Total number of words in the book: {word_count}")
 
 if __name__ == "__main__":
     main()
